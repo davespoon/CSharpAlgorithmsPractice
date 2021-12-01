02d612e73c0291e01910f58e85ed40ba8665c70d
davespoon <dmitriy.grinevich.qa@gmail.com>
2021-12-01 16:53:43 +0200

    Initial commit

---
+++ CSharpAlgorithmsPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpAlgorithmsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CSharpAlgorithmsPractice/Sorting.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharpAlgorithmsPractice
+{
+    public class Sorting
+    {
+        public static void BubbleSort(int[] array)
+        {
+            for (var partIndex = array.Length - 1; partIndex > 0; partIndex--)
+            {
+                for (var i = 0; i < partIndex; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        Swap(array, i, i + 1);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            if (i == j)
+            {
+                return;
+            }
+
+            //swap using deconstruction
+            (array[i], array[j]) = (array[j], array[i]);
+        }
+    }
+}
